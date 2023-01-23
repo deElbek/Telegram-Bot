08b3208567e2e9eb65f1264ced7a46406862a1b9
deElbek <eldorbekqobulov121@gmail.com>
2023-01-23 18:05:37 +0500

    Ishni boshlelar

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-process.env.NODE_ENV!=='production'?require("dotenv").config({path: ".env"}):null;
-const {Telegraf} = require('telegraf')
+process.env.NODE_ENV !== 'production' ? require("dotenv").config({ path: ".env" }) : null;
+const { Telegraf } = require('telegraf')
 const bot = new Telegraf(process.env.TELE_TOKEN)
+require('mongoose').connect(process.env.MongoDB)
+
+// Bot start
+bot.start(msg => {
+
+})
+
+// Bot text
+bot.on('text', msg => {
+
+})
+
+// Bot inlineQuery
+bot.on('callback_query', msg => {
+
+})
+
+// Bot contact
+bot.on("contact", msg => {
+
+})
 
 bot.launch()
